5314be344694d9af853420e3f965e0e8b8a737dd
as4567832 <sachanayush493@gmail.com>
2025-12-30 19:21:58 +0530

    added env

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const chatController = require("../controller/chatController");
 const router = express.Router();
 const multer = require("multer"); // <-- import multer
-const upload = multer({ dest: "uploads/" }); // or memoryStorage 
+const upload = multer({ dest: "tmp/" }); // or memoryStorage 
 const authMiddleWare = require("../middlewares/authMiddleware").authMiddleware;
 router.post("/send-message", authMiddleWare,upload.single("file"), chatController.sendMessage);
 // router.get("/conversations", authMiddleWare, chatController.getAllConversations);
